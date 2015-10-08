6b2a34444849317b97463feb5c0533ad03262a97
Romulo <rcianci@gmail.com>
2015-10-07 21:00:38 -0300

    Código-fonte do exemplo de utilização do StackExchange.Redis publicado no blog http://desenvolvedor.ninja

---
+++ StackExchangeRedisDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace StackExchangeRedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //estabelecer a conexão com Redis
+            using (ConnectionMultiplexer connectionRedis = ConnectionMultiplexer.Connect("pub-redis-13919.us-east-1-2.5.ec2.garantiadata.com:13919,password=salamandra"))
+            {
+                //obter o database para envio de comandos ao Redis
+                IDatabase clientRedis = connectionRedis.GetDatabase();
+                //gravando uma chave
+                clientRedis.StringSet("admin_sistema", "Desenvolvedor Ninja");
+                //lendo uma chave
+                Console.WriteLine(clientRedis.StringGet("admin_sistema"));
+                //definindo 600 segundos como tempo de expiração
+                clientRedis.KeyExpire("admin_sistema", TimeSpan.FromSeconds(600));
+                //consultando o tempo de expiração da chave
+                Console.WriteLine(clientRedis.KeyTimeToLive("admin_sistema"));
+                //retirando o tempo de expiração da chave tornando-a permanente
+                clientRedis.KeyPersist("admin_sistema");
+                //apagando uma chave
+                clientRedis.KeyDelete("admin_sistema");
+                //fechando a conexão com o Redis
+                connectionRedis.Close();
+            }
+        }
+    }
+}
